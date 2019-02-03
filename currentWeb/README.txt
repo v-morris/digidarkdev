b48d3830daa6d1d41f0f782c53310161272bb376
Vicki Morris <41308334+v-morris@users.noreply.github.com>
2019-02-03 13:57:16 -0500

    Add files via upload

---
+++ currentWeb/README.txt
@@ -0,0 +1,9 @@
+BOOTSTRAP LANDING PAGE IN PROGRESS
+
+DAY 3 #100 DAYS OF CODE
+-Practiced making simple layout of a webpage with Bootstrap.
+-Worked with cards, jumbotron, navbar, and scrollspy with sections.
+-Still a work in progress, needs stylized much more.
+
+Sources-
+https://www.udemy.com/bootstrap-4-create-a-landing-page/learn/v4/content
---
+++ currentWeb/js/custom.js
@@ -0,0 +1,21 @@
+var nav = document.getElementById('nav');
+
+window.onscroll = function(){
+    if(window.pageYOffset > 50){
+        nav.style.background = "rgba(0, 48, 87, 0.9)";
+
+    }
+    else{
+        nav.style.background = "transparent"
+    }
+}
+
+$(document).on('click', '[data-toggle="lightbox"]', function (event) {
+    event.preventDefault();
+    $(this).ekkoLightbox();
+});
+
+
+
+
+
